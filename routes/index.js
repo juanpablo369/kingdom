d0d1b041d9a5228abf27d1e6843a533340d89167
negron10 <40868390+negron10@users.noreply.github.com>
2019-07-28 00:23:19 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/principal', function(req, res, next) {
+  res.render('main', { title: 'UNL' });
+});
+
 module.exports = router;
